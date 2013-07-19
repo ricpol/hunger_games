9121c436a9b72810f01ae1830471c207db3005b6
Riccardo Polignieri <ric.pol@libero.it>
2013-07-19 22:52:59 +0200

    rewrite for better log
    now log entries keeps original order of players

--- game.py
+++ game.py
@@ -30,94 +30,100 @@ END_GAME_TRIGGER = 500 # dopo questo turno, innesca una possibilita' di abortire
 END_GAME_CHANCE = .005 # possibilita' che il gioco abortisca subito
 
 
-txt_roundstart = "\nROUND %i; M: %i; players: %s\nreps: %s\nfood: %s\n"
-txt_playerdecision = "player %s, hunts decision %s\n"
-txt_foodearnings = "player %s, food earnings %s\n"
+txt_roundstart = "\nROUND %i      M: %i\nnames: %s\nreps:  %s\nfood:  %s\n"
+txt_playerdecision = "player %s, decision: %s\n"
+txt_foodearnings = "player %s, earns: %s\n"
 txt_award = "award: %i, tot hunters: %i, tot cooperators: %i\n"
 txt_dead = "deads: %s\n"
 txt_summary_log = "\nsummary of deaths:\n%s\n"
 
+class PlayerStats(object):
+    def __init__(self, player):
+        self.hunt_choices = player.hunt_choices
+        self.hunt_outcomes = player.hunt_outcomes
+        self.round_end = player.round_end
+        self.name = player.name
+        self.hunts = 0.0
+        self.slacks = 0.0
+        self.food = 0
+        self.reputation = 1.0
+        
+    def adjust_reputation(self):
+        try: self.reputation = (self.hunts / (self.hunts + self.slacks)) 
+        except ZeroDivisionError: self.reputation = 1.0
+        
+
 class Game(object):
     def __init__(self, players):
-        self.players = players
-        self.nameplayers = [p.name for p in players]
+        self.players = [PlayerStats(p) for p in players]
         self.num_players = len(players)
-        self.hunts = [0.0] * self.num_players
-        self.slacks = [0.0] * self.num_players
-        self.foods = [300*(self.num_players-1)] * self.num_players
+        for p in self.players:
+            p.food = 300*(self.num_players-1)
         self.round = 0
         self.log = open('log.txt', 'a')
-    
-    def _shuffle_players(self):
-        """Dobbiamo dare ai giocatori le liste reputazione in ordine casuale, 
-           ma anche ricordare in che ordine glie le abbiamo date...
-           Per fare in fretta, le diamo a tutti nello stesso ordine
-           (cambiando a ogni turno l'ordine dei giocatori)."""
-        keys = range(self.num_players)
-        random.shuffle(keys)
-        self.players = [self.players[i] for i in keys]
-        self.nameplayers = [self.nameplayers[i] for i in keys]
-        self.hunts = [self.hunts[i] for i in keys]
-        self.slacks = [self.slacks[i] for i in keys]
-        self.foods = [self.foods[i] for i in keys]
-    
+        
     def do_round(self):
         self.round += 1
         # calcolo M
         m = random.randint(1, (self.num_players * (self.num_players-1))-1)
-        # mescolo i giocatori
-        self._shuffle_players()
         # calcolo le reputazioni correnti
-        reps = []
-        for h, s in zip(self.hunts, self.slacks):
-            try: reps.append(h / (h+s))
-            except ZeroDivisionError: reps.append(1.0)
-        self.log.write(txt_roundstart % (self.round, m, str(self.nameplayers), 
-                                         str(reps), str(self.foods)))
+        for p in self.players: 
+            p.adjust_reputation()
+        self.log.write(txt_roundstart % (self.round, m, 
+                                         str([p.name for p in self.players]),
+                                         str([p.reputation for p in self.players]), 
+                                         str([p.food for p in self.players])))
         tot_hunters = 0 # il numero di cacciatori totali (per il cibo premio)
         hunts = [] # matrice di chi caccia con chi
         
         for n, player in enumerate(self.players):
-            reps_2 = reps[:]
-            r = reps_2.pop(n) 
-            hunt = player.hunt_choices(self.round, self.foods[n], r, m, reps_2)
-            self.hunts[n] += hunt.count('h')
-            self.slacks[n] += hunt.count('s')
+            other_reps = [p.reputation for p in self.players if p != player]
+            # mescolo le reputazioni degli altri
+            keys = range(self.num_players-1)
+            random.shuffle(keys)
+            other_reps = [other_reps[i] for i in keys]
+            # chiedo al giocatore le sue decisioni di caccia
+            hunt = player.hunt_choices(self.round, player.food, 
+                                       player.reputation, m, other_reps)
+            player.hunts += hunt.count('h')
+            player.slacks += hunt.count('s')
             tot_hunters += hunt.count('h')
-            hunt.insert(n, None) # re-inserisco None al posto del giocatore corrente
+            # riordino le sue decisioni secondo l'ordine iniziale
+            # (per chiarezza nel log)
+            hunt = [h for k, h in sorted(zip(keys, hunt))]
+            hunt.insert(n, 'X') # un segnaposto per il giocatore corrente
             hunts.append(hunt)
-            self.log.write(txt_playerdecision % (self.nameplayers[n], str(hunt)))
+            self.log.write(txt_playerdecision % (player.name, str(hunt)))
         # distribuisco il cibo a seconda dei risultati della caccia
         tot_cooperators = 0
-        for a in range(self.num_players):
+        for a, player in enumerate(self.players):
             food_earning = []
             for b in range(self.num_players):
-                if hunts[a][b] and hunts[b][a]:
+                if hunts[a][b] != 'X' and hunts[b][a] != 'X':
                     res_a, res_b = FOODS[(hunts[a][b], hunts[b][a])]
                     if hunts[a][b] == hunts[b][a] == 'h':
                         tot_cooperators +=1
-                    self.foods[a] += res_a
+                    player.food += res_a
                     food_earning.append(res_a)
             self.players[a].hunt_outcomes(food_earning)
-            self.log.write(txt_foodearnings % (self.nameplayers[a], str(food_earning)))
+            self.log.write(txt_foodearnings % (self.players[a].name, str(food_earning)))
         # stabilisco se distribuire cibo aggiuntivo:
         if tot_hunters >= m:
             award = 2*(self.num_players-1)
-            self.foods = [i+award for i in self.foods]
+            for player in self.players:
+                player.food += award
         else: 
             award = 0
         self.log.write(txt_award % (award, tot_hunters, tot_cooperators))
         # stabilisco se qualcuno e' morto:
-        deads = [n for n, i in enumerate(self.foods) if i<1]
-        deadnames = [i for i in self.nameplayers if self.nameplayers.index(i) in deads]
+        deadnames = []
+        for player in self.players:
+            if player.food < 1: 
+                deadnames.append(player.name)
         self.log.write(txt_dead % str(deadnames))
-        if deads: 
-            self.num_players -= len(deads)
-            self.players = [i for i in self.players if self.players.index(i) not in deads]
-            self.nameplayers = [i for i in self.nameplayers if self.nameplayers.index(i) not in deads]
-            self.foods = [i for i in self.foods if self.foods.index(i) not in deads]
-            self.hunts = [i for i in self.hunts if self.hunts.index(i) not in deads]
-            self.slacks = [i for i in self.slacks if self.slacks.index(i) not in deads]
+        if deadnames: 
+            self.num_players -= len(deadnames)
+            self.players = [p for p in self.players if p.name not in deadnames]
         # finisco il turno
         for player in self.players:
             player.round_end(award, m, tot_cooperators)
